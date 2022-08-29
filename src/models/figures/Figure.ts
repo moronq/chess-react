07edc5aca002f933d4cea67f940dcf02a4c9601d
Сергей Щербинин <moronqq@gmail.com>
2022-08-29 03:38:06 +0300

    add figure class

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,1 +1,30 @@
-export class Figure {}
+import { Cell } from '../Cell'
+import logo from '../../assets/black-bishop.png'
+import { Colors } from '../Colors'
+
+export enum FigureName {
+  FIGURE = 'Фигура',
+  KING = 'Король',
+  KNIGHT = 'Конь',
+  PAWN = 'Пешка',
+  QUEEN = 'Ферзь',
+  ROOK = 'Ладья',
+  BISHOP = 'Слон',
+}
+
+export class Figure {
+  cell: Cell
+  logo: typeof logo | null
+  color: Colors
+  name: FigureName
+  id: number
+
+  constructor(color: Colors, cell: Cell) {
+    this.color = color
+    this.cell = cell
+    this.cell.figure = this
+    this.logo = null
+    this.name = FigureName.FIGURE
+    this.id = Math.random()
+  }
+}
