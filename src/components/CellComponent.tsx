785e19e7d0d675cabee028b94ae53c3b98ae58c4
Сергей Щербинин <moronqq@gmail.com>
2022-08-29 05:06:42 +0300

    add movement to figures

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -3,10 +3,21 @@ import { Cell } from '../models/Cell'
 
 interface CellProps {
   cell: Cell
+  selected: boolean
+  click: (cell: Cell) => void
 }
 
-const CellComponent: FC<CellProps> = ({ cell }) => {
-  return <div className={['cell', cell.color].join(' ')}></div>
+const CellComponent: FC<CellProps> = ({ cell, selected, click }) => {
+  return (
+    <div
+      onClick={() => click(cell)}
+      className={['cell', cell.color, selected ? 'selected' : ''].join(' ')}
+      style={{ background: cell.available && cell.figure ? 'green' : '' }}
+    >
+      {cell.available && !cell.figure && <div className="available" />}
+      {cell.figure?.logo && <img src={cell.figure.logo} alt="figure" />}
+    </div>
+  )
 }
 
 export default CellComponent
